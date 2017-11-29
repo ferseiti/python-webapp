ff6d40a6212c3b2b8524d071839bc4f5f4feba9c
Fernando Seiti Furusato <ferseiti@gmail.com>
2017-11-29 00:33:33 -0200

    More modifications towards a functioning python webapp

--- containers/views.py
+++ containers/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the containers index.")
+    something = 'Fernando Furusato'
+
+    arguments = { 'myName': something,
+                  'age': 32 }
+    return render(request, 'containers/test.html', arguments)
+    
+    #return HttpResponse("Hello, world. You're at the containers index.")
--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '/home/ferseiti/django-tutorial/containers/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
